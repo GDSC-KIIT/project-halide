3f57b7d82b4133871427de6b00f885cf925d674f
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-09-03 04:17:01 +0530

    Indented the code properly

--- src/system.cpp
+++ src/system.cpp
@@ -2,9 +2,9 @@
 
 unsigned char inportb (unsigned short _port)
 {
-    	unsigned char rv;
-    	__asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
-    	return rv;
+    unsigned char rv;
+    __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
+    return rv;
 }
 
 void outportb (unsigned short _port, unsigned char _data)
