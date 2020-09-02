011a9919b959e6e713c1da6a584a782638f9c385
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-09-03 03:03:42 +0530

    Changed all definations to declarations

--- include/frame_buffer.h
+++ include/frame_buffer.h
@@ -41,21 +41,9 @@ namespace FrameBuffer
 		unsigned char bg;
 
 	public:
-		Writer(const unsigned char &foreground, const unsigned char &background)
-		{
-			fg = foreground;
-			bg = background;
-		}
+		Writer(const unsigned char &foreground, const unsigned char &background);
 
-		void print(char *str)
-		{
-			char *vid_mem = START;
-			for (int i = 0; str[i] != '\0'; i++)
-			{
-				vid_mem[i * 2] = str[i];
-				vid_mem[(i * 2) + 1] = ((bg & 0x0f) << 4) | (fg & 0x0f);
-			}
-		}
+		void print(char *str);
 	};
 } // namespace FrameBuffer
 
