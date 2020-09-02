b2115d9d9087c3c3f340f727e0bdea43c1168753
Junaid Rahim <junaidrahim8d@gmail.com>
2020-09-03 02:27:08 +0530

    fg bg bug fix

--- include/frame_buffer.hpp
+++ include/frame_buffer.hpp
@@ -36,7 +36,7 @@ namespace FrameBuffer
 			for (int i = 0; str[i] != '\0'; i++)
 			{
 				vid_mem[i * 2] = str[i];
-				vid_mem[(i * 2) + 1] = ((fg & 0x0f) << 4) | (bg & 0x0f);
+				vid_mem[(i * 2) + 1] = ((bg & 0x0f) << 4) | (fg & 0x0f);
 			}
 		}
 	};
