40225d171982b7f46142ee1035e45be8e8720e0b
Junaid Rahim <junaidrahim8d@gmail.com>
2020-09-04 04:34:13 +0530

    ASCII Art

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -14,9 +14,36 @@ extern "C" void call_ctors()
 
 extern "C" void k_main(const void *multiboot_structure, unsigned int multiboot_magic)
 {
-	FrameBuffer::Writer p(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::BLUE);
-	p.writeString("HalideOS, an experimental OS by DSC KIIT\n");
+	FrameBuffer::Writer p(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::BLACK);
+
+	// p.writeString("\n\n\n\n\n\n");
+	// p.writeString("       \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\n");
+	// p.writeString("       \b  \b\b\b\b  \b\b\b\b\b\b\b\b\b  \b\b\b\b\b\b\b\b\b  \b\b\b\b\b\b\b\b\b\b\b    \b\b\b\b\b      \b\b\n");
+	// p.writeString("       \b  \b\b\b\b  \b\b\b\b\b\b\b\b\b  \b\b\b\b\b\b\b\b\b  \b\b\b\b\b\b\b\b\b\b  \b\b  \b\b\b  \b\b\b\b  \b\n");
+	// p.writeString("       \b  \b\b\b\b  \b\b\b\b\b\b\b\b\b  \b\b\b\b\b\b\b\b\b  \b\b\b\b\b\b\b\b\b  \b\b\b\b  \b\b  \b\b\b\b  \b\n");
+	// p.writeString("       \b  \b\b\b\b  \b\b\b   \b\b\b  \b\b  \b\b\b\b\b  \b\b\b   \b\b\b  \b\b\b\b  \b\b\b  \b\b\b\b\b\b\n");
+	// p.writeString("       \b        \b\b  \b  \b\b  \b\b\b\b\b\b\b    \b\b  \b  \b\b  \b\b\b\b  \b\b\b\b\b  \b\b\b\b\n");
+	// p.writeString("       \b  \b\b\b\b  \b\b\b\b\b  \b\b  \b\b  \b\b  \b  \b\b     \b\b  \b\b\b\b  \b\b\b\b\b\b\b  \b\b\n");
+	// p.writeString("       \b  \b\b\b\b  \b\b\b    \b\b  \b\b  \b\b  \b  \b\b  \b\b\b\b\b  \b\b\b\b  \b\b  \b\b\b\b  \b\n");
+	// p.writeString("       \b  \b\b\b\b  \b\b  \b  \b\b  \b\b  \b\b  \b  \b\b  \b  \b\b\b  \b\b  \b\b\b  \b\b\b\b  \b\n");
+	// p.writeString("       \b  \b\b\b\b  \b\b\b    \b\b  \b\b  \b\b\b    \b\b\b   \b\b\b\b\b    \b\b\b\b\b      \b\b\n");
+	// p.writeString("       \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\n");
+
+	p.writeString("\n\n\n\n\n\n");
+	p.writeString("       \f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\n");
+	p.writeString("       \f  \f\f\f\f  \f\f\f\f\f\f\f\f\f  \f\f\f\f\f\f\f\f\f  \f\f\f\f\f\f\f\f\f\f\f    \f\f\f\f\f      \f\f\n");
+	p.writeString("       \f  \f\f\f\f  \f\f\f\f\f\f\f\f\f  \f\f\f\f\f\f\f\f\f  \f\f\f\f\f\f\f\f\f\f  \f\f  \f\f\f  \f\f\f\f  \f\n");
+	p.writeString("       \f  \f\f\f\f  \f\f\f\f\f\f\f\f\f  \f\f\f\f\f\f\f\f\f  \f\f\f\f\f\f\f\f\f  \f\f\f\f  \f\f  \f\f\f\f  \f\n");
+	p.writeString("       \f  \f\f\f\f  \f\f\f   \f\f\f  \f\f  \f\f\f\f\f  \f\f\f   \f\f\f  \f\f\f\f  \f\f\f  \f\f\f\f\f\f\n");
+	p.writeString("       \f        \f\f  \f  \f\f  \f\f\f\f\f\f\f    \f\f  \f  \f\f  \f\f\f\f  \f\f\f\f\f  \f\f\f\f\n");
+	p.writeString("       \f  \f\f\f\f  \f\f\f\f\f  \f\f  \f\f  \f\f  \f  \f\f     \f\f  \f\f\f\f  \f\f\f\f\f\f\f  \f\f\n");
+	p.writeString("       \f  \f\f\f\f  \f\f\f    \f\f  \f\f  \f\f  \f  \f\f  \f\f\f\f\f  \f\f\f\f  \f\f  \f\f\f\f  \f\n");
+	p.writeString("       \f  \f\f\f\f  \f\f  \f  \f\f  \f\f  \f\f  \f  \f\f  \f  \f\f\f  \f\f  \f\f\f  \f\f\f\f  \f\n");
+	p.writeString("       \f  \f\f\f\f  \f\f\f    \f\f  \f\f  \f\f\f    \f\f\f   \f\f\f\f\f    \f\f\f\f\f      \f\f\n");
+	p.writeString("       \f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\n");
+
 	KEYBOARD_DRIVER::readInput(p);
 
-	while (1);
+	while (1)
+		;
 }
