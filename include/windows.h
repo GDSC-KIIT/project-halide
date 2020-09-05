4e7286b953829164c63de7366a4f029649ead3b6
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-09-06 01:53:51 +0530

    Added window class in headers

--- include/windows.h
+++ include/windows.h
@@ -4,8 +4,9 @@
 class Window {
     public:
     int m_x1, m_x2, m_y1, m_y2;
-    Window(int x1, int x2, int y1, int y2)
-        : m_x1(x1), m_x2(x2), m_y1(y1), m_y2(y2) {}
+    char* m_title;
+    Window(int x1, int x2, int y1, int y2, char* title)
+        : m_x1(x1), m_x2(x2), m_y1(y1), m_y2(y2), m_title(title) {}
 };
 
 #endif
