b72651ae05dcf63f319ed8112fbecaab8064032b
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-09-07 05:21:46 +0530

    debugging logic added

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -15,44 +15,69 @@ extern "C" void call_ctors()
 	}
 }
 
+static void initScreen(FrameBuffer::Writer& p, Window& win)
+{
+	int x = win.m_y2;
+	x=win.m_y2-1;
+	p.writeString(win.m_title);
+	p.fillRemeaning("=", true);
+	p.write_at_index(x);
+}
+
 extern "C" void k_main(const void *multiboot_structure, unsigned int multiboot_magic)
 {
 	Window win1(10,70,0,9, (char*)"Basic window title v.0.2 instance 1 instance_id(a.0.1)");
 	FrameBuffer::Writer p(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::RED, win1);
 
-	int x = win1.m_y2;
+	initScreen(p, win1);
+	/* int x = win1.m_y2;
 	x=win1.m_y2-1;
 	p.writeString(win1.m_title);
 	p.fillRemeaning("=", true);
-	p.write_at_index(x);
+	p.write_at_index(x); */
 
 	Window win2(10,70,9,16, (char*)"Basic window title v.0.2 instance 2 instance_id(b.0.1)");
 	FrameBuffer::Writer p1(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::BLUE, win2);
 
-	int y = win2.m_y2;
+	initScreen(p1, win2);
+	/* int y = win2.m_y2;
 	y=win2.m_y2-1;
 	p1.writeString(win2.m_title);
 	p1.fillRemeaning("=", true);
-	p1.write_at_index(y);
+	p1.write_at_index(y); */
 
-	/* Window win3(10,70,16,25, (char*)"Basic window title v.0.2 instance 3 instance_id(c.0.1)");
-	FrameBuffer::Writer p3(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::GREEN, win3);
+	Window win3(10,70,16,25, (char*)"Basic window title v.0.2 instance 3 instance_id(c.0.1)");
+	FrameBuffer::Writer p2(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::GREEN, win3);
 
-	int z = win3.m_y2;
+	initScreen(p2, win3);
+	/* int z = win3.m_y2;
 	z=win3.m_y2-1;
-	p3.writeString(win3.m_title);
-	p3.fillRemeaning("=", true);
-	p3.write_at_index(z); */
+	p2.writeString(win3.m_title);
+	p2.fillRemeaning("=", true);
+	p2.write_at_index(z); */
 
 	#if USE_BOOT_SCREEN_1 == 1
 		#include "../include/bootscreen1.h"
 	#elif USE_BOOT_SCREEN_1 == 0
 		#include "../include/bootscreen2.h"
 	#endif
-
+	
+	p.switchWindow(p);
 	while(true)
 	{
-	char* input_buffer = KEYBOARD_DRIVER::readInput(p1);
+	char* input_buffer = KEYBOARD_DRIVER::readInput(p);
+	//p.writeString(input_buffer);	
+	}
+	p1.switchWindow(p1);
+	while(true)
+	{
+	char* input_buffer = KEYBOARD_DRIVER::readInput(p);
+	//p.writeString(input_buffer);	
+	}
+	p2.switchWindow(p2);
+	while(true)
+	{
+	char* input_buffer = KEYBOARD_DRIVER::readInput(p);
 	//p.writeString(input_buffer);	
 	}
 
