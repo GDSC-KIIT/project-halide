e1365f02c5b180ee4d6904871a29cf9c79177c76
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-09-06 15:21:43 +0530

    Made some mior changes in kernel_file cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -17,7 +17,7 @@ extern "C" void call_ctors()
 
 extern "C" void k_main(const void *multiboot_structure, unsigned int multiboot_magic)
 {
-	Window win1(10,70,0,10, (char*)"Basic window title v.0.1 instance 1");
+	Window win1(10,70,0,8, (char*)"Basic window title v.0.2 instance 1 instance_id(a.0.1)");
 	FrameBuffer::Writer p(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::BLACK, win1);
 
 	int x = win1.m_y2;
@@ -26,7 +26,7 @@ extern "C" void k_main(const void *multiboot_structure, unsigned int multiboot_m
 	p.fillRemeaning("=", true);
 	p.write_at_index(x);
 
-	Window win2(10,70,11,20, (char*)"Basic window title v.0.1 instance 2");
+	Window win2(10,70,9,16, (char*)"Basic window title v.0.2 instance 2 instance_id(b.0.1)");
 	FrameBuffer::Writer p1(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::BLACK, win2);
 
 	int y = win2.m_y2;
@@ -34,6 +34,15 @@ extern "C" void k_main(const void *multiboot_structure, unsigned int multiboot_m
 	p1.writeString(win2.m_title);
 	p1.fillRemeaning("=", true);
 	p1.write_at_index(y);
+
+	Window win3(10,70,17,25, (char*)"Basic window title v.0.2 instance 3 instance_id(c.0.1)");
+	FrameBuffer::Writer p3(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::BLACK, win3);
+
+	int z = win3.m_y2;
+	z=win3.m_y2-1;
+	p3.writeString(win3.m_title);
+	p3.fillRemeaning("=", true);
+	p3.write_at_index(z);
 
 	#if USE_BOOT_SCREEN_1 == 1
 		#include "../include/bootscreen1.h"
@@ -44,7 +53,7 @@ extern "C" void k_main(const void *multiboot_structure, unsigned int multiboot_m
 	while(true)
 	{
 	char* input_buffer = KEYBOARD_DRIVER::readInput(p);
-	p.writeString(input_buffer);	
+	//p.writeString(input_buffer);	
 	}
 
 	
