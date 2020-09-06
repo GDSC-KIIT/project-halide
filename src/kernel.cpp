82986bfb2fd7a1171d2ddd4c8b2ecb34c70ef047
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-09-06 14:44:55 +0530

    Removed windows bugs and added proper syntax to instantiate windows in console

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -17,24 +17,37 @@ extern "C" void call_ctors()
 
 extern "C" void k_main(const void *multiboot_structure, unsigned int multiboot_magic)
 {
-	Window win1(10,70,5,20, (char*)"Basic window title v.0.1");
+	Window win1(10,70,0,10, (char*)"Basic window title v.0.1 instance 1");
 	FrameBuffer::Writer p(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::BLACK, win1);
+
+	int x = win1.m_y2;
+	x=win1.m_y2-1;
+	p.writeString(win1.m_title);
+	p.fillRemeaning("=", true);
+	p.write_at_index(x);
+
+	Window win2(10,70,11,20, (char*)"Basic window title v.0.1 instance 2");
+	FrameBuffer::Writer p1(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::BLACK, win2);
+
+	int y = win2.m_y2;
+	y=win2.m_y2-1;
+	p1.writeString(win2.m_title);
+	p1.fillRemeaning("=", true);
+	p1.write_at_index(y);
 
 	#if USE_BOOT_SCREEN_1 == 1
 		#include "../include/bootscreen1.h"
 	#elif USE_BOOT_SCREEN_1 == 0
 		#include "../include/bootscreen2.h"
 	#endif
-	int x = win1.m_y2;
-	x=win1.m_y2-1;
-	p.writeString(win1.m_title);
-	p.fillRemeaning("=", true);
-	p.write_at_index(x);
+
 	while(true)
 	{
 	char* input_buffer = KEYBOARD_DRIVER::readInput(p);
 	p.writeString(input_buffer);	
 	}
+
+	
 	while (1);
 }
 
