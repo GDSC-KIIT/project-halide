1cd3a268fcaa47b33b874b7d4f041f7d0845103b
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-09-07 07:14:08 +0530

    Added password hiding mode

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,11 +1,11 @@
 #ifndef __KEYBOARD_H
 #define __KEYBOARD_H
 
-#include "../include/frame_buffer.h"
+#include <frame_buffer.h>
 
 namespace KEYBOARD_DRIVER
 {
-	char *readInput(FrameBuffer::Writer &p);
+	char *readInput(FrameBuffer::Writer &p, int mode=1);
 }
 
 #endif
