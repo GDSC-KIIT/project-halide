ef637c3f064dcd7677e5df6c74983c0dfb86e35b
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-09-07 05:43:24 +0530

    Started work on halide standard library

---
+++ include/halidestdlib.h
@@ -0,0 +1,8 @@
+#ifndef __HALIDE_STD_LIB
+#define __HALIDE_STD_LIB
+
+namespace hldstd {
+    int stringCompare(char*, char*);
+}
+
+#endif
---
+++ src/halidestdlib.cpp
@@ -0,0 +1,8 @@
+#include "../include/halidestdlib.h"
+
+int hldstd::stringCompare(char* s1, char* s2)
+{
+    bool eq=false;
+    // Logic
+    return (eq) ? 1 : 0;
+}
