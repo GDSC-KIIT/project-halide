38276c474b4cede0dc46df7ecfa689f4dfd500bc
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-09-07 12:12:50 +0530

    Archive folder cleanup

--- Archives/v0.0.2/include/system.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SYSTEM_H
-#define __SYSTEM_H
-
-unsigned char inportb (unsigned short _port);
-
-void outportb (unsigned short _port, unsigned char _data);
-
-#endif
--- Archives/v0.0.2/src/utils.cpp
+++
@@ -1,6 +0,0 @@
-#include <utils.h>
-
-void *mem_alloc(int nbytes) {
-	char variable[200];
-	return &variable;
-}
