1fb70de0b2ef84e5deb050c2bcb6369bb60e2cd9
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-09-07 23:13:27 +0530

    broken

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -32,34 +32,19 @@ static void initScreen(FrameBuffer::Writer &p, Window &win) {
 }
 
 extern struct mem_mgr *new_mem_mgr(unsigned long start, unsigned long sz);
-extern void set_framebuffer(unsigned int*);
+extern void set_framebuffer(unsigned int *);
 
-void setup_memmgr(unsigned int *multiboot)
-{
-	unsigned int* memupper = (unsigned int*)multiboot[2];
-	new_mem_mgr(*memupper, 64*1048576);
+void setup_memmgr(unsigned int *multiboot) {
+	unsigned int *memupper = (unsigned int *)multiboot[2];
+	new_mem_mgr(*memupper, 64 * 1048576);
 }
 
-		// while (true) {
-
-		// }
-		// p1.switchWindow(p1);
-		// while (true) {
-		// 	char *input_buffer = KEYBOARD_DRIVER::readInput(p1);
-		// 	break;
-		// }
-		// p2.switchWindow(p2);
-		// while (true) {
-		// 	char *input_buffer = KEYBOARD_DRIVER::readInput(p2);
-		// 	break;
-		// }
-	}
-
+extern "C" void k_main(unsigned int multiboot) {
 	// window_t *wnd = window(desktop, "Hello world", 12, 10, 1000, 600);
 	// wnd->border_color = 0xff000000;
 	// wnd->background_color = 0xff00ffff;
 	// wnd->draw(wnd);
-	
+
 	// // * instantiate globaldescriptortable here
 	// GLOBAL_DESCRIPTOR_TABLE::GlobalDescriptorTable globaldescriptortable;
 	// {
@@ -88,37 +73,37 @@ void setup_memmgr(unsigned int *multiboot)
 	// 		timer--;
 	// 	} */
 	// }
-// 
+	//
 	// Window win1(10, 70, 0, 9, (char *)"Basic window title v.0.2 instance 1 instance_id(a.0.1)");
 	// FrameBuffer::Writer p(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::RED, win1);
-// 
+	//
 	// initScreen(p, win1);
 	// /* int x = win1.m_y2;
 	// x=win1.m_y2-1;
 	// p.writeString(win1.m_title);
 	// p.fillRemeaning("=", true);
 	// p.write_at_index(x); */
-// 
+	//
 	// Window win2(10, 70, 9, 16, (char *)"Basic window title v.0.2 instance 2 instance_id(b.0.1)");
 	// FrameBuffer::Writer p1(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::BLUE, win2);
-// 
+	//
 	// initScreen(p1, win2);
 	// /* int y = win2.m_y2;
 	// y=win2.m_y2-1;
 	// p1.writeString(win2.m_title);
 	// p1.fillRemeaning("=", true);
 	// p1.write_at_index(y); */
-// 
+	//
 	// Window win3(10, 70, 16, 25, (char *)"Basic window title v.0.2 instance 3 instance_id(c.0.1)");
 	// FrameBuffer::Writer p2(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::GREEN, win3);
-// 
+	//
 	// initScreen(p2, win3);
 	// /* int z = win3.m_y2;
 	// z=win3.m_y2-1;
 	// p2.writeString(win3.m_title);
 	// p2.fillRemeaning("=", true);
 	// p2.write_at_index(z); */
-// 
+	//
 	// /* #if USE_BOOT_SCREEN_1 == 1
 	// 	#include "../include/bootscreen1.h"
 	// #elif USE_BOOT_SCREEN_1 == 0
@@ -141,5 +126,6 @@ void setup_memmgr(unsigned int *multiboot)
 	// 		break;
 	// 	}
 	// }
-	while (1);
+	while (1)
+		;
 }
