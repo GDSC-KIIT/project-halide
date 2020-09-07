e2e36f2070b5dc5ac93e30b095bf0c05cb077ef5
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-09-07 07:18:36 +0530

    added function defination for stringLength and implimented string_compare

--- src/halidestdlib.cpp
+++ src/halidestdlib.cpp
@@ -1,8 +1,27 @@
 #include <halidestdlib.h>
 
+int hldstd::stringLength(char* str)
+{
+    int len=0;
+    while(str[len++]);
+    return len-1;
+}
+
 int hldstd::stringCompare(char* s1, char* s2)
 {
     bool eq=false;
-    // Logic
+    if(stringLength(s1) != stringLength(s2))
+        eq = false;
+    for (int i = 0; s1[i] != '\0'; i++)
+    {
+        if(s1[i] != s2[i]) {
+            eq= true;
+            break;
+        }
+        else {
+            eq = true;
+        }
+    }
+    
     return (eq) ? 1 : 0;
 }
