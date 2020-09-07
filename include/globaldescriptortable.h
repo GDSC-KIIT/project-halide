18f8145fa4509d630cee90a8e8f9a140eed44734
Junaid Rahim <junaidrahim8d@gmail.com>
2020-09-07 12:21:19 +0530

    formatting fix

--- include/globaldescriptortable.h
+++ include/globaldescriptortable.h
@@ -2,33 +2,34 @@
 #define __GLOBAL_DESCRIPTOR_TABLE_H
 
 namespace GLOBAL_DESCRIPTOR_TABLE {
-    class GlobalDescriptorTable {
-        public:
-        class SegmentDescriptor {
-            private:
-            unsigned short lower_limit;
-            unsigned short lower_base;
-            unsigned char higher_base;
-            unsigned char type;
-            unsigned char flags_higher_limit;
-            unsigned char base_vhi;
+	class GlobalDescriptorTable {
+	public:
+		class SegmentDescriptor {
+		private:
+			unsigned short lower_limit;
+			unsigned short lower_base;
+			unsigned char higher_base;
+			unsigned char type;
+			unsigned char flags_higher_limit;
+			unsigned char base_vhi;
 
-            public:
-            SegmentDescriptor(unsigned int base, unsigned int limit, unsigned char type);
-            unsigned int Base();
-            unsigned int Limit();
-        }__attribute__((packed));
-        SegmentDescriptor null;
-        SegmentDescriptor unused;
-        SegmentDescriptor code;
-        SegmentDescriptor data;
-        public:
-        GlobalDescriptorTable();
-        ~GlobalDescriptorTable();
-        unsigned short codeSegment();
-        unsigned short dataSegment();
-    };
-}
+		public:
+			SegmentDescriptor(unsigned int base, unsigned int limit, unsigned char type);
+			unsigned int Base();
+			unsigned int Limit();
+		} __attribute__((packed));
+		SegmentDescriptor null;
+		SegmentDescriptor unused;
+		SegmentDescriptor code;
+		SegmentDescriptor data;
+
+	public:
+		GlobalDescriptorTable();
+		~GlobalDescriptorTable();
+		unsigned short codeSegment();
+		unsigned short dataSegment();
+	};
+} // namespace GLOBAL_DESCRIPTOR_TABLE
 
 #endif
 
