7fdc99e4aa457951ca775b305df3d056520cc133
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-09-09 00:00:35 +0530

    Modified main kernel to accomodate window destruction and rerendering (hard coded and conditionally)

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -31,13 +31,13 @@ extern "C" void k_main() {
 	GLOBAL_DESCRIPTOR_TABLE::GlobalDescriptorTable globaldescriptortable;
 	{
 		Window win;
-		FrameBuffer::Writer p(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::LIGHT_BLUE, win);
+		FrameBuffer::Writer p(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::LIGHT_BLUE, &win);
 		initScreen(p, win);
-		#if USE_BOOT_SCREEN_1 == 1
-		#include "../include/bootscreen1.h"
-		#elif USE_BOOT_SCREEN_1 == 0
-		#include "../include/bootscreen2.h"
-		#endif
+#if USE_BOOT_SCREEN_1 == 1
+#include "../include/bootscreen1.h"
+#elif USE_BOOT_SCREEN_1 == 0
+#include "../include/bootscreen2.h"
+#endif
 		p.writeString((char *)"\n\n\n\n\nEnter password : ");
 		while (true) {
 			char *input_buffer = KEYBOARD_DRIVER::readInput(p, 0);
@@ -57,30 +57,42 @@ extern "C" void k_main() {
 	}
 
 	Window win1(10, 70, 0, 9, (char *)"Basic window title v.0.2 instance 1 instance_id(a.0.1)");
-	FrameBuffer::Writer p(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::LIGHT_BLUE, win1);
+	FrameBuffer::Writer p(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::LIGHT_BLUE, &win1);
 	win1.storeBuffer(0, 9);
-
 	initScreen(p, win1);
 
 	Window win2(10, 70, 9, 16, (char *)"Basic window title v.0.2 instance 2 instance_id(b.0.1)");
-	FrameBuffer::Writer p1(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::LIGHT_RED, win2);
+	FrameBuffer::Writer p1(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::LIGHT_RED, &win2);
 	initScreen(p1, win2);
 	win2.storeBuffer(9, 16);
 
 	Window win3(10, 70, 16, 25, (char *)"Basic window title v.0.2 instance 3 instance_id(c.0.1)");
-	FrameBuffer::Writer p2(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::LIGHT_CYAN, win3);
+	FrameBuffer::Writer p2(FrameBuffer::Colours::WHITE, FrameBuffer::Colours::LIGHT_CYAN, &win3);
 	initScreen(p2, win3);
 	win3.storeBuffer(16, 25);
-	
+
 	while (true) {
 		p.switchWindow(p);
 		int x = init_console(p, win1);
+		if (x == 2) {
+			win3.DestroyWindow(&win2);
+			p1.Rerender(&win2);
+			x = 1;
+		}
+		if (x == 3) {
+			win2.DestroyWindow(&win1);
+			p.Rerender(&win1);
+			x = 1;
+		}
 		if (x == 1) {
-			p1.switchWindow(p1);
-
-			int y = init_console(p1, win2);
-			p2.switchWindow(p2);
-			int z = init_console(p2, win3);
+			if (win2.isDestroyed == false) {
+				p1.switchWindow(p1);
+				int y = init_console(p1, win2);
+			}
+			if ((win3.isDestroyed == false)) {
+				p2.switchWindow(p2);
+				int z = init_console(p2, win3);
+			}
 		}
 	}
 	while (1)
