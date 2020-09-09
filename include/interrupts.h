173d3c91ffc92152d9ca179c0970eaeeec0ec807
Junaid Rahim <junaidrahim8d@gmail.com>
2020-09-09 20:15:25 +0530

    code cleanup

--- include/interrupts.h
+++ include/interrupts.h
@@ -16,7 +16,6 @@ protected:
 	} __attribute__((packed));
 
 	static GateDescriptor interruptDescriptorTable[256];
-
 	struct interruptDescriptorTablePointer {
 		unsigned char size;
 		unsigned int base;
@@ -28,14 +27,16 @@ protected:
 	Port8BitSlow picMasterData;
 	Port8BitSlow picSlaveCommand;
 	Port8BitSlow picSlaveData;
-	
+
 public:
 	InterruptManager(GLOBAL_DESCRIPTOR_TABLE::GlobalDescriptorTable *globalDescriptorTable);
-	~InterruptManager();
+	
 	void Activate();
 	static unsigned int HandleInterrupt(unsigned char interruptNumber, unsigned int esp);
 	static void IgnoreInterruptRequest();
 	static void HandleInterruptRequest0x00();
 	static void HandleInterruptRequest0x01();
+	
+	~InterruptManager();
 };
 #endif
